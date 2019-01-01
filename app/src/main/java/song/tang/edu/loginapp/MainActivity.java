c85b6f7a748cdc3c56fc5d041042faa8ec9a7ab5
1298se <oliversong7h@gmail.com>
2019-01-01 15:42:30 -0500

    Initial commit

---
+++ app/src/main/java/song/tang/edu/loginapp/MainActivity.java
@@ -0,0 +1,16 @@
+package song.tang.edu.loginapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+// PseudoCode
+// Title
+// Dropdown Menu for Region
+// Popup Information
+// Log In Button
+// Sign Up
+// Text Fields for Registration
