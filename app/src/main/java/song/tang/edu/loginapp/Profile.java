6aaa51ef67c2ce3b927bbe6e8c3a7cd50ca8fb0e
1298se <oliversong7h@gmail.com>
2019-01-04 19:06:25 -0500

    Added rounded buttons

--- app/src/main/java/song/tang/edu/loginapp/Profile.java
+++ app/src/main/java/song/tang/edu/loginapp/Profile.java
@@ -1,8 +1,15 @@
 package song.tang.edu.loginapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,37 +17,85 @@ import android.widget.TextView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import org.w3c.dom.Text;
+
 public class Profile extends AppCompatActivity {
 
-    private TextView profileTextView;
-    private Button signOut;
+    private DrawerLayout drawer;
     private FirebaseAuth firebaseAuth;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+    private TextView userTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        profileTextView = (TextView) findViewById(R.id.profileTextView);
-        signOut = (Button) findViewById(R.id.signOutButton);
+        // Init
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         firebaseAuth = FirebaseAuth.getInstance();
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
 
+        // Set toolbar as actionbar
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_closed);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Firebase
         if(firebaseAuth.getCurrentUser() == null){
             finish();
             startActivity(new Intent(getApplicationContext(), Login.class));
         }
 
+        // Set user's name in nav header
         FirebaseUser user = firebaseAuth.getCurrentUser();
+        View headerView = navigationView.getHeaderView(0);
+        userTextView = (TextView) headerView.findViewById(R.id.userTextView);
+        userTextView.setText(user.getDisplayName());
 
-        profileTextView.setText(user.getDisplayName());
+        // Change fragments in navigation drawer
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch(menuItem.getItemId()) {
+                    case R.id.nav_home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                new HomeFragment()).commit();
+                        break;
 
-        signOut.setOnClickListener(new View.OnClickListener() {
+                    case R.id.nav_profile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                new UserProfileFragment()).commit();
+                        break;
+                }
+                drawer.closeDrawer(GravityCompat.START);
+
+                return true;
+            }
+        });
+
+        /*signOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 firebaseAuth.signOut();
                 finish();
                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
             }
-        });
+        });*/
     }
+
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
 }
