62e0528c589eeac2dc92142fc6904dcfaa599a0a
1298se <oliversong7h@gmail.com>
2019-01-20 13:18:24 -0500

    building database structure

--- app/src/main/java/song/tang/edu/loginapp/EditUserProfile.java
+++ app/src/main/java/song/tang/edu/loginapp/EditUserProfile.java
@@ -19,7 +19,6 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.google.android.gms.auth.api.signin.internal.Storage;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
